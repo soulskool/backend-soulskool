7b4585fbf5cea164aecc39b48d644ef20124ab35
Piyush Kumar <piyushofficial101103@gmail.com>
2025-03-22 21:40:28 +0530

    routes

--- routes/referralRoutes.js
+++ routes/referralRoutes.js
@@ -1,14 +1,3 @@
-// import express from 'express';
-// import {generateReferralLink} from '../controllers/referralController';
-// import {authenticate} from '../middleware/auth';
-
-// const router = express.Router();
-
-// router.get('/generate', authenticate, generateReferralLink);
-
-// export default router;
-
-
 import express from "express";
 import { generateReferralLink } from "../controllers/referralController.js";
 import { authenticate } from "../middleware/auth.js";
